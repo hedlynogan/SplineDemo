69aec9807759ddeb4aeb818ac6b77feef468c261
Ned Hogan <nedhogan@me.com>
2024-02-29 21:13:32 -0800

    Init of Sean Allan Spline Demo

---
+++ Spline/Spline/SplineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplineApp.swift
+//  Spline
+//
+//  Created by Edward Hogan on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SplineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
