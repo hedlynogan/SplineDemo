46182124c2c295beb624279912a9cfdde92be282
Ned Hogan <nedhogan@me.com>
2024-02-29 21:18:53 -0800

    Update to start onbaording 3d spline model

--- Spline/Spline/ContentView.swift
+++ Spline/Spline/ContentView.swift
@@ -10,14 +10,31 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Rectangle()
+                .foregroundStyle(.secondary)
+                .frame(height: 500)
+                .ignoresSafeArea()
+            VStack(spacing: 12) {
+                Text("Build Your Brand")
+                    .font(.title.bold())
+                Text("Stay up to date with all your social media platforms in one simple app.")
+                    .foregroundStyle(.secondary)
+            }
+            .padding(.horizontal)
+            Spacer()
+            Button("Get Started") {
+                
+            }
+            .buttonStyle(.borderedProminent)
+            
         }
         .padding()
+        .tint(.green)
+        
+        Spacer()
     }
 }
+
 
 #Preview {
     ContentView()
